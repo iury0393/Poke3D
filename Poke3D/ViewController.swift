cf4b0f080c81c888c1bef844d85eeddbb4991a19
Iury Vasconcelos <iury0393@gmail.com>
2022-10-05 10:34:51 -0300

    Tracking multiple Pokemons

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -70,10 +70,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             node.addChildNode(planeNode)
             
-            if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
-                if let pokeNode = pokeScene.rootNode.childNodes.first {
-                    pokeNode.eulerAngles.x = -.pi / 2
-                    planeNode.addChildNode(pokeNode)
+            if imageAnchor.referenceImage.name == "eevee-card" {
+                if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        pokeNode.eulerAngles.x = -.pi / 2
+                        planeNode.addChildNode(pokeNode)
+                    }
+                }
+            }
+            
+            if imageAnchor.referenceImage.name == "oddish-card" {
+                if let pokeScene = SCNScene(named: "art.scnassets/oddish.scn") {
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        pokeNode.eulerAngles.x = -.pi / 2
+                        planeNode.addChildNode(pokeNode)
+                    }
                 }
             }
         }
